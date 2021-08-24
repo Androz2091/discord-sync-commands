da7ca5145c20079c0636d23ccee023ae66fa384f
Androz2091 <androz2091@gmail.com>
2021-08-24 10:54:23 +0200

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Discord Sync Commands
+
+Easily keep your bot's slash commands synchronized with Discord! 🔁
+
+## Features
+
+⚡ No useless calls to the Discord API!
+❗ Auto detection of changes in your commands!
+🤟 Super easy to use!
+
+## Example
+
+```js
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    intents: []
+});
+
+const synchronizeSlashCommands = require('discord-sync-commands');
+synchronizeSlashCommands(client, [
+    {
+        name: 'ping',
+        description: 'Check whether the bot is working'
+    }
+]);
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', (interaction) => {
+
+    if (!interaction.isCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        interaction.reply('Pong!');
+    }
+
+});
+
+client.login('token');
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, commands, debug) => {
+
+    const log = (message) => debug && console.log(message);
+
+    const ready = client.readyAt ? Promise.resolve() : new Promise(resolve => client.once('ready', resolve));
+    await ready;
+    await client.application.commands.fetch();
+
+    log(`Synchronizing commands...`);
+    log(`Currently ${client.application.commands.cache.size} commands.`);
+
+    const newCommands = commands.filter((command) => !client.application.commands.cache.some((c) => c.name === command.name));
+    for (let newCommand of newCommands) {
+        await client.application.commands.create(newCommand);
+    }
+
+    log(`Created ${newCommands.length} commands!`);
+
+    const deletedCommands = client.application.commands.cache.filter((command) => !commands.some((c) => c.name === command.name));
+    for (let deletedCommand of deletedCommands) {
+        await deletedCommand.delete();
+    }
+
+    log(`Deleted ${deletedCommands.size} commands!`);
+
+    const updatedCommands = commands.filter((command) => client.application.commands.cache.some((c) => c.name === command.name));
+    let updatedCommandCount = 0;
+    for (let updatedCommand of updatedCommands) {
+        const newCommand = updatedCommand;
+        const previousCommand = client.application.commands.cache.find((c) => c.name === updatedCommand.name);
+        let modified = false;
+        if (previousCommand.description !== newCommand.description) modified = true;
+        if (!Discord.ApplicationCommand.optionsEqual(previousCommand.options ?? [], newCommand.options ?? [])) modified = true;
+        if (modified) {
+            await previousCommand.edit(newCommand);
+            updatedCommandCount++;
+        }
+    }
+
+    log(`Updated ${updatedCommandCount} commands!`);
+
+    log(`Commands synchronized!`);
+
+};
